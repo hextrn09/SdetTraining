f8be2a3a9ccdd995936369964ea6e191c1597acb
Your Name <you@example.com>
2016-10-21 15:24:57 -0400

    first test java file

---
+++ JenkinsGitEx1.java
@@ -0,0 +1,14 @@
+package com.fanniemae;
+
+public class JenkinsGitEx1{
+	public static void main(String[] args) {
+		System.out.println("Hiiiiii Welcome to Git and Jenkins");
+		
+		addNumbers(1,2);		
+	}
+	
+	public static void addNumbers(int a, int b){
+		System.out.println("Result of adding " + a + " and " + b + " is: " + (a+b));
+	}
+
+}
