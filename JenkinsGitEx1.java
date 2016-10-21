e50c33265c545ba61fc9e937a26e9f93df4a59cf
Your Name <you@example.com>
2016-10-21 16:23:15 -0400

    Added Subtract method

--- JenkinsGitEx1.java
+++ JenkinsGitEx1.java
@@ -1,14 +1,17 @@
-package com.fanniemae;
-
 public class JenkinsGitEx1{
 	public static void main(String[] args) {
 		System.out.println("Hiiiiii Welcome to Git and Jenkins");
 		
-		addNumbers(1,2);		
+		addNumbers(1,2);
+			subtractNumbers(5,3);
 	}
 	
 	public static void addNumbers(int a, int b){
 		System.out.println("Result of adding " + a + " and " + b + " is: " + (a+b));
+	}
+	
+	public static void subtractNumbers(int a, int b){
+		System.out.println("Result of adding " + a + " and " + b + " is: " + (a-b));
 	}
 
 }
